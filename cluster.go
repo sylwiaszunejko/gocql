db99b709103aa4e306dead75874f62b8a8c3a898
sylwiaszunejko <sylwia.szunejko@gmail.com>
2023-10-10 14:58:58 +0200

    stream observer

--- cluster.go
+++ cluster.go
@@ -17,6 +17,8 @@ import (
 type PoolConfig struct {
 	// HostSelectionPolicy sets the policy for selecting which host to use for a
 	// given query (default: RoundRobinHostPolicy())
+	// It is not supported to use a single HostSelectionPolicy in multiple sessions
+	// (even if you close the old session before using in a new session).
 	HostSelectionPolicy HostSelectionPolicy
 }
 
@@ -190,6 +192,10 @@ type ClusterConfig struct {
 	// FrameHeaderObserver will set the provided frame header observer on all frames' headers created from this session.
 	// Use it to collect metrics / stats from frames by providing an implementation of FrameHeaderObserver.
 	FrameHeaderObserver FrameHeaderObserver
+
+	// StreamObserver will be notified of stream state changes.
+	// This can be used to track in-flight protocol requests and responses.
+	StreamObserver StreamObserver
 
 	// Default idempotence for queries
 	DefaultIdempotence bool
