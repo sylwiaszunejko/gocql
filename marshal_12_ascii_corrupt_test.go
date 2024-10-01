45f5c56c5b06ca2aaadefe919e067256d838570a
illia-li <jaforja@gmail.com>
2024-10-01 18:20:36 -0400

    add `ascii` tests

---
+++ marshal_12_ascii_corrupt_test.go
@@ -0,0 +1,46 @@
+package gocql_test
+
+import (
+	"testing"
+
+	"github.com/gocql/gocql"
+	"github.com/gocql/gocql/internal/tests/serialization"
+	"github.com/gocql/gocql/internal/tests/serialization/mod"
+)
+
+func TestMarshalAsciiMustFail(t *testing.T) {
+	tType := gocql.NewNativeType(4, gocql.TypeAscii, "")
+
+	marshal := func(i interface{}) ([]byte, error) {
+		return gocql.Marshal(tType, i)
+	}
+	unmarshal := func(bytes []byte, i interface{}) error {
+		return gocql.Unmarshal(tType, bytes, i)
+	}
+
+	// according to the 'cql protocol' - 'ascii' a sequence of bytes in the ASCII range [0, 127].
+	// marshal and unmarshal functions does not return an error if bytes have values outside of this range.
+	brokenAllTypes := serialization.GetTypes(mod.Values{[]byte{}, ""}.AddVariants(mod.All...)...)
+
+	serialization.NegativeMarshalSet{
+		Values: mod.Values{
+			[]byte{255},
+			[]byte{127, 255, 127},
+			string([]byte{255}),
+			string([]byte{127, 255, 127}),
+		}.AddVariants(mod.All...),
+		BrokenTypes: brokenAllTypes,
+	}.Run("corrupt_vals", t, marshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data:        []byte{255},
+		Values:      mod.Values{[]byte{}, ""}.AddVariants(mod.All...),
+		BrokenTypes: brokenAllTypes,
+	}.Run("corrupt_data1", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data:        []byte{127, 255, 127},
+		Values:      mod.Values{[]byte{}, ""}.AddVariants(mod.All...),
+		BrokenTypes: brokenAllTypes,
+	}.Run("corrupt_data2", t, unmarshal)
+}
---
+++ marshal_12_ascii_test.go
@@ -0,0 +1,48 @@
+package gocql_test
+
+import (
+	"testing"
+
+	"github.com/gocql/gocql"
+	"github.com/gocql/gocql/internal/tests/serialization"
+	"github.com/gocql/gocql/internal/tests/serialization/mod"
+)
+
+func TestMarshalAscii(t *testing.T) {
+	tType := gocql.NewNativeType(4, gocql.TypeAscii, "")
+
+	marshal := func(i interface{}) ([]byte, error) {
+		return gocql.Marshal(tType, i)
+	}
+	unmarshal := func(bytes []byte, i interface{}) error {
+		return gocql.Unmarshal(tType, bytes, i)
+	}
+
+	// unmarshal `zero` data into ([]byte)(nil), (*[]byte)(*[nil])
+	brokenZeroSlices := serialization.GetTypes(make([]byte, 0), (*[]byte)(nil))
+
+	serialization.PositiveSet{
+		Data: nil,
+		Values: mod.Values{
+			([]byte)(nil),
+			(*[]byte)(nil),
+			(*string)(nil),
+		}.AddVariants(mod.CustomType),
+	}.Run("[nil]nullable", t, marshal, unmarshal)
+
+	serialization.PositiveSet{
+		Data:   nil,
+		Values: mod.Values{""}.AddVariants(mod.CustomType),
+	}.Run("[nil]unmarshal", t, nil, unmarshal)
+
+	serialization.PositiveSet{
+		Data:                 make([]byte, 0),
+		Values:               mod.Values{make([]byte, 0), ""}.AddVariants(mod.All...),
+		BrokenUnmarshalTypes: brokenZeroSlices,
+	}.Run("[]unmarshal", t, nil, unmarshal)
+
+	serialization.PositiveSet{
+		Data:   []byte("test text string"),
+		Values: mod.Values{[]byte("test text string"), "test text string"}.AddVariants(mod.All...),
+	}.Run("text", t, nil, unmarshal)
+}
