d947f1094a2500a8623bb3252f016eeee5d639b4
illia-li <jaforja@gmail.com>
2024-10-21 15:10:15 -0400

    add `map` tests

---
+++ tests/serialization/marshal_20_map_v3_corrupt_test.go
@@ -0,0 +1,139 @@
+package serialization_test
+
+import (
+	"math"
+	"testing"
+
+	"github.com/gocql/gocql"
+	"github.com/gocql/gocql/internal/tests/serialization"
+	"github.com/gocql/gocql/internal/tests/serialization/mod"
+)
+
+func TestMarshalMapV3Corrupt(t *testing.T) {
+	elem := gocql.NewNativeType(3, gocql.TypeSmallInt, "")
+	tType := gocql.NewCollectionType(gocql.NewNativeType(3, gocql.TypeMap, ""), elem, elem)
+
+	//unmarshal data than bigger the normal data, does not return error.
+	brokenBigData := serialization.GetTypes(mod.Values{
+		make(map[int16]int16), make(map[int16]*int16),
+		make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+	}.AddVariants(mod.All...)...)
+
+	refInt32 := func(v int32) *int32 { return &v }
+	refModInt32 := func(v mod.Int32) *mod.Int32 { return &v }
+
+	marshal := func(i interface{}) ([]byte, error) { return gocql.Marshal(tType, i) }
+	unmarshal := func(bytes []byte, i interface{}) error {
+		return gocql.Unmarshal(tType, bytes, i)
+	}
+
+	val := int32(math.MaxInt16 + 1)
+	valc := mod.Int32(val)
+	serialization.NegativeMarshalSet{
+		Values: mod.Values{
+			map[int32]int32{val: val}, map[int32]int32{val: 0}, map[int32]int32{0: val},
+			map[int32]*int32{val: refInt32(val)}, map[int32]*int32{val: refInt32(0)}, map[int32]*int32{0: refInt32(val)},
+			map[mod.Int32]mod.Int32{valc: valc}, map[mod.Int32]mod.Int32{valc: 0}, map[mod.Int32]mod.Int32{0: valc},
+			map[mod.Int32]*mod.Int32{valc: refModInt32(valc)}, map[mod.Int32]*mod.Int32{valc: refModInt32(0)}, map[mod.Int32]*mod.Int32{0: refModInt32(valc)},
+		}.AddVariants(mod.All...),
+	}.Run("big_vals", t, marshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00\x00\x02\xff\xff\x00\x00\x00\x02\xff\xff\x01"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+		BrokenTypes: brokenBigData,
+	}.Run("big_data_elem1+", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\xff"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+		BrokenTypes: brokenBigData,
+	}.Run("big_data_zeroElem1+", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00\x00\x00\x01"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+		BrokenTypes: brokenBigData,
+	}.Run("big_data_elems0+", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00\x00\x02\xff\xff\x00\x00\x00\x02\xff"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+	}.Run("small_data_val_value-", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00\x00\x02\xff\xff\x00\x00\x00\x02"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+	}.Run("small_data_val_len", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00\x00\x02\xff\xff\x00\x00"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+	}.Run("small_data_val_len-", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00\x00\x02\xff\xff"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+	}.Run("small_data_val-", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00\x00\x02\xff"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+	}.Run("small_data_key_value-", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00\x00\x02"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+	}.Run("small_data_key_len", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+	}.Run("small_data_key_len-", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00\x00\x01"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+	}.Run("small_data_pair-", t, unmarshal)
+
+	serialization.NegativeUnmarshalSet{
+		Data: []byte("\x00\x00"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+	}.Run("small_data_elems-", t, unmarshal)
+}
---
+++ tests/serialization/marshal_20_map_v3_test.go
@@ -0,0 +1,64 @@
+package serialization_test
+
+import (
+	"testing"
+
+	"github.com/gocql/gocql"
+	"github.com/gocql/gocql/internal/tests/serialization"
+	"github.com/gocql/gocql/internal/tests/serialization/mod"
+)
+
+func TestMarshalMapV3(t *testing.T) {
+	elem := gocql.NewNativeType(3, gocql.TypeSmallInt, "")
+	tType := gocql.NewCollectionType(gocql.NewNativeType(3, gocql.TypeMap, ""), elem, elem)
+
+	refInt16 := func(v int16) *int16 { return &v }
+	refModInt16 := func(v mod.Int16) *mod.Int16 { return &v }
+
+	marshal := func(i interface{}) ([]byte, error) { return gocql.Marshal(tType, i) }
+	unmarshal := func(bytes []byte, i interface{}) error {
+		return gocql.Unmarshal(tType, bytes, i)
+	}
+
+	serialization.PositiveSet{
+		Data: nil,
+		Values: mod.Values{
+			(map[int16]int16)(nil), (map[int16]*int16)(nil),
+			(map[mod.Int16]mod.Int16)(nil), (map[mod.Int16]*mod.Int16)(nil),
+			(*map[int16]int16)(nil), (*map[int16]*int16)(nil),
+			(*map[mod.Int16]mod.Int16)(nil), (*map[mod.Int16]*mod.Int16)(nil),
+		}.AddVariants(mod.CustomType),
+	}.Run("[nil]nullable", t, marshal, unmarshal)
+
+	serialization.PositiveSet{
+		Data: []byte("\x00\x00\x00\x00"),
+		Values: mod.Values{
+			make(map[int16]int16), make(map[int16]*int16),
+			make(map[mod.Int16]mod.Int16), make(map[mod.Int16]*mod.Int16),
+		}.AddVariants(mod.All...),
+	}.Run("zero elems", t, marshal, unmarshal)
+
+	serialization.PositiveSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00"),
+		Values: mod.Values{
+			map[int16]int16{0: 0}, map[int16]*int16{0: refInt16(0)},
+			map[mod.Int16]mod.Int16{0: 0}, map[mod.Int16]*mod.Int16{0: refModInt16(0)},
+		}.AddVariants(mod.All...),
+	}.Run("[]{zero elem}unmarshal", t, nil, unmarshal)
+
+	serialization.PositiveSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00\x00\x00\x02\x00\x00"),
+		Values: mod.Values{
+			map[int16]int16{0: 0}, map[int16]*int16{0: refInt16(0)},
+			map[mod.Int16]mod.Int16{0: 0}, map[mod.Int16]*mod.Int16{0: refModInt16(0)},
+		}.AddVariants(mod.All...),
+	}.Run("[]{0:0}", t, marshal, unmarshal)
+
+	serialization.PositiveSet{
+		Data: []byte("\x00\x00\x00\x01\x00\x00\x00\x02\x7f\xff\x00\x00\x00\x02\x7f\xff"),
+		Values: mod.Values{
+			map[int16]int16{32767: 32767}, map[int16]*int16{32767: refInt16(32767)},
+			map[mod.Int16]mod.Int16{32767: 32767}, map[mod.Int16]*mod.Int16{32767: refModInt16(32767)},
+		}.AddVariants(mod.All...),
+	}.Run("[]{max:max}", t, marshal, unmarshal)
+}
