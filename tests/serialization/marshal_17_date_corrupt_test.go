8a691c064e2cdfd0a1cad81a9adda7fb4efc988a
illia-li <jaforja@gmail.com>
2024-12-11 16:55:27 -0400

    fix `date` tests

--- tests/serialization/marshal_17_date_corrupt_test.go
+++ tests/serialization/marshal_17_date_corrupt_test.go
@@ -10,88 +10,101 @@ import (
 	"github.com/gocql/gocql"
 	"github.com/gocql/gocql/internal/tests/serialization"
 	"github.com/gocql/gocql/internal/tests/serialization/mod"
+	"github.com/gocql/gocql/serialization/date"
 )
 
 func TestMarshalDateCorrupt(t *testing.T) {
 	tType := gocql.NewNativeType(4, gocql.TypeDate, "")
 
-	marshal := func(i interface{}) ([]byte, error) { return gocql.Marshal(tType, i) }
-	unmarshal := func(bytes []byte, i interface{}) error {
-		return gocql.Unmarshal(tType, bytes, i)
+	type testSuite struct {
+		name      string
+		marshal   func(interface{}) ([]byte, error)
+		unmarshal func(bytes []byte, i interface{}) error
 	}
 
-	// marshal the `int64`, `time.Time` values which out of the `cql type` range, does not return an error.
-	brokenMarshalTypes := serialization.GetTypes(int64(0), (*int64)(nil), time.Time{}, &time.Time{})
+	testSuites := [2]testSuite{
+		{
+			name:      "serialization.date",
+			marshal:   date.Marshal,
+			unmarshal: date.Unmarshal,
+		},
+		{
+			name: "glob",
+			marshal: func(i interface{}) ([]byte, error) {
+				return gocql.Marshal(tType, i)
+			},
+			unmarshal: func(bytes []byte, i interface{}) error {
+				return gocql.Unmarshal(tType, bytes, i)
+			},
+		},
+	}
 
-	// unmarshal of `string`, `time.Time` does not return an error on all type of data corruption.
-	brokenUnmarshalTypes := serialization.GetTypes(string(""), (*string)(nil), time.Time{}, &time.Time{})
+	for _, tSuite := range testSuites {
+		marshal := tSuite.marshal
+		unmarshal := tSuite.unmarshal
 
-	serialization.NegativeMarshalSet{
-		Values: mod.Values{
-			time.Date(5881580, 7, 12, 0, 0, 0, 0, time.UTC).UTC().UnixMilli(),
-			time.Date(5881580, 8, 11, 0, 0, 0, 0, time.UTC).UTC().UnixMilli(),
-			time.Date(5881581, 7, 11, 0, 0, 0, 0, time.UTC).UTC().UnixMilli(),
-			time.Date(5883581, 12, 20, 0, 0, 0, 0, time.UTC).UTC().UnixMilli(),
-			"5881580-07-12", "5881580-08-11", "5881581-07-11", "9223372036854775807-07-12",
-			time.Date(5881580, 7, 12, 0, 0, 0, 0, time.UTC).UTC(),
-			time.Date(5881580, 8, 11, 0, 0, 0, 0, time.UTC).UTC(),
-			time.Date(5881581, 7, 11, 0, 0, 0, 0, time.UTC).UTC(),
-			time.Date(5883581, 12, 20, 0, 0, 0, 0, time.UTC).UTC(),
-		}.AddVariants(mod.All...),
-		BrokenTypes: brokenMarshalTypes,
-	}.Run("big_vals", t, marshal)
+		t.Run(tSuite.name, func(t *testing.T) {
+			serialization.NegativeMarshalSet{
+				Values: mod.Values{
+					time.Date(5881580, 7, 12, 0, 0, 0, 0, time.UTC).UnixMilli(),
+					time.Date(5881580, 8, 11, 0, 0, 0, 0, time.UTC).UnixMilli(),
+					time.Date(5881581, 7, 11, 0, 0, 0, 0, time.UTC).UnixMilli(),
+					time.Date(5883581, 12, 20, 0, 0, 0, 0, time.UTC).UnixMilli(),
+					"5881580-07-12", "5881580-08-11", "5881581-07-11", "9223372036854775807-07-12",
+					time.Date(5881580, 7, 12, 0, 0, 0, 0, time.UTC).UTC(),
+					time.Date(5881580, 8, 11, 0, 0, 0, 0, time.UTC).UTC(),
+					time.Date(5881581, 7, 11, 0, 0, 0, 0, time.UTC).UTC(),
+					time.Date(5883581, 12, 20, 0, 0, 0, 0, time.UTC).UTC(),
+				}.AddVariants(mod.All...),
+			}.Run("big_vals", t, marshal)
 
-	serialization.NegativeMarshalSet{
-		Values: mod.Values{
-			time.Date(-5877641, 06, 24, 0, 0, 0, 0, time.UTC).UTC().UnixMilli(),
-			time.Date(-5877641, 07, 23, 0, 0, 0, 0, time.UTC).UTC().UnixMilli(),
-			time.Date(-5877642, 06, 23, 0, 0, 0, 0, time.UTC).UTC().UnixMilli(),
-			time.Date(-5887641, 06, 23, 0, 0, 0, 0, time.UTC).UTC().UnixMilli(),
-			"5881580-07-12", "5881580-08-11", "5881581-07-11", "9223372036854775807-07-12",
-			"-5877641-06-24", "-5877641-07-23", "-5877642-06-23", "-9223372036854775807-07-12",
-			time.Date(-5877641, 06, 24, 0, 0, 0, 0, time.UTC).UTC(),
-			time.Date(-5877641, 07, 23, 0, 0, 0, 0, time.UTC).UTC(),
-			time.Date(-5877642, 06, 23, 0, 0, 0, 0, time.UTC).UTC(),
-			time.Date(-5887641, 06, 23, 0, 0, 0, 0, time.UTC).UTC(),
-		}.AddVariants(mod.All...),
-		BrokenTypes: brokenMarshalTypes,
-	}.Run("small_vals", t, marshal)
+			serialization.NegativeMarshalSet{
+				Values: mod.Values{
+					time.Date(-5877641, 06, 22, 0, 0, 0, 0, time.UTC).UnixMilli(),
+					time.Date(-5877641, 05, 23, 0, 0, 0, 0, time.UTC).UnixMilli(),
+					time.Date(-5877642, 06, 23, 0, 0, 0, 0, time.UTC).UnixMilli(),
+					time.Date(-5887641, 06, 23, 0, 0, 0, 0, time.UTC).UnixMilli(),
+					"-5877641-06-22", "-5877641-05-23", "-5877642-06-23", "-9223372036854775807-07-12",
+					time.Date(-5877641, 06, 22, 0, 0, 0, 0, time.UTC),
+					time.Date(-5877641, 05, 23, 0, 0, 0, 0, time.UTC),
+					time.Date(-5877642, 06, 23, 0, 0, 0, 0, time.UTC),
+					time.Date(-5887641, 06, 23, 0, 0, 0, 0, time.UTC),
+				}.AddVariants(mod.All...),
+			}.Run("small_vals", t, marshal)
 
-	serialization.NegativeMarshalSet{
-		Values: mod.Values{
-			"a1580-07-11", "1970-0d-11", "02-11", "1970-11",
-		}.AddVariants(mod.All...),
-	}.Run("corrupt_vals", t, marshal)
+			serialization.NegativeMarshalSet{
+				Values: mod.Values{
+					"a1580-07-11", "1970-0d-11", "02-11", "1970-11",
+				}.AddVariants(mod.All...),
+			}.Run("corrupt_vals", t, marshal)
 
-	serialization.NegativeUnmarshalSet{
-		Data: []byte("\x00\x00\x00\x00\x00"),
-		Values: mod.Values{
-			int64(0), time.Time{}, "",
-		}.AddVariants(mod.All...),
-		BrokenTypes: brokenUnmarshalTypes,
-	}.Run("big_data1", t, unmarshal)
+			serialization.NegativeUnmarshalSet{
+				Data: []byte("\x00\x00\x00\x00\x00"),
+				Values: mod.Values{
+					int64(0), time.Time{}, "",
+				}.AddVariants(mod.All...),
+			}.Run("big_data1", t, unmarshal)
 
-	serialization.NegativeUnmarshalSet{
-		Data: []byte("\x00\x00\x4e\x94\x91\x4e\xff\xff\xff"),
-		Values: mod.Values{
-			int64(0), time.Time{}, "",
-		}.AddVariants(mod.All...),
-		BrokenTypes: brokenUnmarshalTypes,
-	}.Run("big_data2", t, unmarshal)
+			serialization.NegativeUnmarshalSet{
+				Data: []byte("\x00\x00\x4e\x94\x91\x4e\xff\xff\xff"),
+				Values: mod.Values{
+					int64(0), time.Time{}, "",
+				}.AddVariants(mod.All...),
+			}.Run("big_data2", t, unmarshal)
 
-	serialization.NegativeUnmarshalSet{
-		Data: []byte("\x00\x00\x00"),
-		Values: mod.Values{
-			int64(0), time.Time{}, "",
-		}.AddVariants(mod.All...),
-		BrokenTypes: brokenUnmarshalTypes,
-	}.Run("small_data1", t, unmarshal)
+			serialization.NegativeUnmarshalSet{
+				Data: []byte("\x00\x00\x00"),
+				Values: mod.Values{
+					int64(0), time.Time{}, "",
+				}.AddVariants(mod.All...),
+			}.Run("small_data1", t, unmarshal)
 
-	serialization.NegativeUnmarshalSet{
-		Data: []byte("\x00"),
-		Values: mod.Values{
-			int64(0), time.Time{}, "",
-		}.AddVariants(mod.All...),
-		BrokenTypes: brokenUnmarshalTypes,
-	}.Run("small_data2", t, unmarshal)
+			serialization.NegativeUnmarshalSet{
+				Data: []byte("\x00"),
+				Values: mod.Values{
+					int64(0), time.Time{}, "",
+				}.AddVariants(mod.All...),
+			}.Run("small_data2", t, unmarshal)
+		})
+	}
 }
--- tests/serialization/marshal_17_date_test.go
+++ tests/serialization/marshal_17_date_test.go
@@ -4,88 +4,92 @@
 package serialization_test
 
 import (
+	"math"
 	"testing"
 	"time"
 
 	"github.com/gocql/gocql"
 	"github.com/gocql/gocql/internal/tests/serialization"
 	"github.com/gocql/gocql/internal/tests/serialization/mod"
+	"github.com/gocql/gocql/serialization/date"
 )
 
 func TestMarshalsDate(t *testing.T) {
 	tType := gocql.NewNativeType(4, gocql.TypeDate, "")
 
-	marshal := func(i interface{}) ([]byte, error) { return gocql.Marshal(tType, i) }
-	unmarshal := func(bytes []byte, i interface{}) error {
-		return gocql.Unmarshal(tType, bytes, i)
+	type testSuite struct {
+		name      string
+		marshal   func(interface{}) ([]byte, error)
+		unmarshal func(bytes []byte, i interface{}) error
+	}
+
+	testSuites := [2]testSuite{
+		{
+			name:      "serialization.date",
+			marshal:   date.Marshal,
+			unmarshal: date.Unmarshal,
+		},
+		{
+			name: "glob",
+			marshal: func(i interface{}) ([]byte, error) {
+				return gocql.Marshal(tType, i)
+			},
+			unmarshal: func(bytes []byte, i interface{}) error {
+				return gocql.Unmarshal(tType, bytes, i)
+			},
+		},
 	}
 
 	zeroDate := time.Date(-5877641, 06, 23, 0, 0, 0, 0, time.UTC).UTC()
 	middleDate := time.UnixMilli(0).UTC()
 	maxDate := time.Date(5881580, 07, 11, 0, 0, 0, 0, time.UTC).UTC()
 
-	// marshal strings with big year like "-5877641-06-23" returns an error
-	brokenBigString := serialization.GetTypes(string(""), (*string)(nil))
+	for _, tSuite := range testSuites {
+		marshal := tSuite.marshal
+		unmarshal := tSuite.unmarshal
 
-	// marshal `custom string` and `custom int64` unsupported
-	brokenMarshal := serialization.GetTypes(mod.String(""), (*mod.String)(nil), mod.Int64(0), (*mod.Int64)(nil))
+		t.Run(tSuite.name, func(t *testing.T) {
+			serialization.PositiveSet{
+				Data: nil,
+				Values: mod.Values{
+					(*uint32)(nil), (*int32)(nil), (*int64)(nil), (*string)(nil), (*time.Time)(nil),
+				}.AddVariants(mod.CustomType),
+			}.Run("[nil]nullable", t, marshal, unmarshal)
 
-	// unmarshal `zero` data into not zero string and time.Time
-	brokenZero := serialization.GetTypes(time.Time{}, &time.Time{}, string(""), (*string)(nil))
+			serialization.PositiveSet{
+				Data: nil,
+				Values: mod.Values{
+					uint32(0), int32(0), zeroDate.UnixMilli(), "", zeroDate,
+				}.AddVariants(mod.CustomType),
+			}.Run("[nil]unmarshal", t, nil, unmarshal)
 
-	// unmarshal `nil` data into not zero time.Time
-	brokenNil := serialization.GetTypes(time.Time{})
+			serialization.PositiveSet{
+				Data: make([]byte, 0),
+				Values: mod.Values{
+					uint32(0), int32(0), zeroDate.UnixMilli(), zeroDate, "-5877641-06-23",
+				}.AddVariants(mod.All...),
+			}.Run("[]unmarshal", t, nil, unmarshal)
 
-	// unmarshal into `custom string`, `int64` and `custom int64` unsupported
-	brokenUnmarshal := serialization.GetTypes(mod.String(""), (*mod.String)(nil), mod.Int64(0), (*mod.Int64)(nil), int64(0), (*int64)(nil))
+			serialization.PositiveSet{
+				Data: []byte("\x00\x00\x00\x00"),
+				Values: mod.Values{
+					uint32(0), int32(0), zeroDate.UnixMilli(), zeroDate, "-5877641-06-23",
+				}.AddVariants(mod.All...),
+			}.Run("zeros", t, marshal, unmarshal)
 
-	serialization.PositiveSet{
-		Data: nil,
-		Values: mod.Values{
-			(*int64)(nil), (*time.Time)(nil), (*string)(nil),
-		}.AddVariants(mod.CustomType),
-	}.Run("[nil]nullable", t, marshal, unmarshal)
+			serialization.PositiveSet{
+				Data: []byte("\x80\x00\x00\x00"),
+				Values: mod.Values{
+					uint32(1 << 31), int32(math.MinInt32), middleDate.UnixMilli(), middleDate, "1970-01-01",
+				}.AddVariants(mod.All...),
+			}.Run("middle", t, marshal, unmarshal)
 
-	serialization.PositiveSet{
-		Data: nil,
-		Values: mod.Values{
-			int64(0), zeroDate, "",
-		}.AddVariants(mod.CustomType),
-		BrokenUnmarshalTypes: append(brokenUnmarshal, brokenNil...),
-	}.Run("[nil]unmarshal", t, nil, unmarshal)
-
-	serialization.PositiveSet{
-		Data: make([]byte, 0),
-		Values: mod.Values{
-			int64(0), zeroDate, "-5877641-06-23",
-		}.AddVariants(mod.All...),
-		BrokenUnmarshalTypes: append(brokenUnmarshal, brokenZero...),
-	}.Run("[]unmarshal", t, nil, unmarshal)
-
-	serialization.PositiveSet{
-		Data: []byte("\x00\x00\x00\x00"),
-		Values: mod.Values{
-			zeroDate.UnixMilli(), zeroDate, "-5877641-06-23",
-		}.AddVariants(mod.All...),
-		BrokenMarshalTypes:   append(brokenMarshal, brokenBigString...),
-		BrokenUnmarshalTypes: brokenUnmarshal,
-	}.Run("zeros", t, marshal, unmarshal)
-
-	serialization.PositiveSet{
-		Data: []byte("\x80\x00\x00\x00"),
-		Values: mod.Values{
-			middleDate.UnixMilli(), middleDate, "1970-01-01",
-		}.AddVariants(mod.All...),
-		BrokenMarshalTypes:   brokenMarshal,
-		BrokenUnmarshalTypes: brokenUnmarshal,
-	}.Run("middle", t, marshal, unmarshal)
-
-	serialization.PositiveSet{
-		Data: []byte("\xff\xff\xff\xff"),
-		Values: mod.Values{
-			maxDate.UnixMilli(), maxDate, "5881580-07-11",
-		}.AddVariants(mod.All...),
-		BrokenMarshalTypes:   append(brokenMarshal, brokenBigString...),
-		BrokenUnmarshalTypes: brokenUnmarshal,
-	}.Run("max", t, marshal, unmarshal)
+			serialization.PositiveSet{
+				Data: []byte("\xff\xff\xff\xff"),
+				Values: mod.Values{
+					uint32(math.MaxUint32), int32(-1), maxDate.UnixMilli(), maxDate, "5881580-07-11",
+				}.AddVariants(mod.All...),
+			}.Run("max", t, marshal, unmarshal)
+		})
+	}
 }
